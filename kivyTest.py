dac49549e8c4e8fb73f64141d709caba55471d3a
ekinder02 <110261879+ekinder02@users.noreply.github.com>
2023-10-17 09:01:00 -0500

    almost done

--- kivyTest.py
+++ kivyTest.py
@@ -1,11 +1,10 @@
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.image import Image,AsyncImage
 from kivy.animation import Animation
 from kivy.uix.button import Button
 import random
 from kivy.config import Config
+from kivy.uix.progressbar import ProgressBar
+from kivy.uix.widget import Widget
 import time
 
 Config.set('graphics', 'width', '960')
@@ -13,35 +12,56 @@ Config.set('graphics', 'height', '540')
 Config.write()
 
 tickles = 0
+canClick = True
+lastClick = 0
         
 class LeTickleApp(App): 
     def build(self): 
+        return(LebronLayout())
+
+          
+class LebronLayout(Widget):
+    def __init__(self, **args):
+        super(LebronLayout, self).__init__(**args)
+    
+        pb = ProgressBar(max=100,
+                        center = (480,400),
+                        size = (500, 500))
+        pb.value = tickles
+        self.add_widget(pb)
+    
         btn = Button(text ="",
-                     color =(1, 0, .65, 1),
-                     background_normal = 'lebron.png',
-                     background_down ='lebronBlush4.png',
-                     size_hint = (.3, .3),
-                     pos = (0,0),
-                   ) 
-        btn.bind(on_press = lambda x: self.callback(btn))
-        return btn 
-    def callback(self,btn):
-        animation = Animation(pos=(random.randint(0,960),random.randint(0,540)), t='in_out_back')
-        animation.start(btn)
-        global tickles
-        tickles += 1
-        if tickles > 5 and tickles <= 10:
-            btn.background_normal = 'lebronBlush1.png'
-        elif tickles > 10 and tickles <= 15:
-            btn.background_normal = 'lebronBlush2.png'
-        elif tickles > 15 and tickles <= 20:
-            btn.background_normal = 'lebronBlush3.png'
-        elif tickles > 20:
-            btn.background_normal = 'lebronBlush4.png'
-            
+                        color =(1, 0, .65, 1),
+                        background_normal = 'lebron.png',
+                        background_down ='lebronBlush4.png',
+                        size = (250, 250),
+                        pos = (0,0),
+                    ) 
+        btn.bind(on_press = lambda x: self.callback(btn,pb))
+        self.add_widget(btn)
     
+    def callback(self,btn,pb):
+        global canClick
+        global lastClick
+        currentTime = time.time()
+        if currentTime - lastClick > 5:
+            canClick = True
+        if canClick == True:
+            animation = Animation(pos=(random.randint(0,960),random.randint(0,400)), t='in_out_back',d = 0.5)
+            animation.start(btn)
+            pb.value += 5
+            if pb.value > 25 and pb.value <= 50:
+                btn.background_normal = 'lebronBlush1.png'
+            elif pb.value > 50 and pb.value <= 75:
+                btn.background_normal = 'lebronBlush2.png'
+            elif pb.value > 75 and pb.value <= 100:
+                btn.background_normal = 'lebronBlush3.png'
+            elif pb.value > 100:
+                btn.background_normal = 'lebronBlush4.png'
+            lastClick = time.time()
+            canClick = False
+
 root = LeTickleApp() 
-    
 # run function runs the whole program 
 # i.e run() method which calls the target 
 # function passed to the constructor. 
